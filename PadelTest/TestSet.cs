dd131b2050bd733b47df6a9b519678be9c9902d9
MariaKilsved <mkilsved@gmail.com>
2021-12-01 10:05:25 +0100

    Re-push of the same changes (merge problems)

--- PadelTest/TestSet.cs
+++ PadelTest/TestSet.cs
@@ -8,6 +8,51 @@ namespace PadelTest
 {
     public class TestSet
     {
+
+
+        [Fact]
+        public void Set_Init3_NoPoints()
+        {
+            Player player1 = new Player("Player 1");
+            Player player2 = new Player("Player 2");
+
+            Set mySet = new Set(3, player1, player2);
+
+            Assert.Equal(0, mySet.Points(player1));
+            Assert.Equal(0, mySet.Points(player2));
+        }
+
+        [Fact]
+        public void Set_Simulate3Games_Player1Wins()
+        {
+            Player player1 = new Player("Player 1");
+            Player player2 = new Player("Player 2");
+
+            Set mySet = new Set(3, player1, player2);
+
+            mySet[0].Point(player1); // 15 - 0
+            mySet[0].Point(player1); // 30 - 0
+            mySet[0].Point(player2); // 30 - 15
+            mySet[0].Point(player1); // 40 - 15
+            mySet[0].Point(player1); // Player 1 vinner Gamet
+
+            mySet[1].Point(player1); // 15 - 0
+            mySet[1].Point(player1); // 30 - 0
+            mySet[1].Point(player2); // 30 - 15
+            mySet[1].Point(player1); // 40 - 15
+            mySet[1].Point(player1); // Player 1 vinner Gamet
+
+            mySet[2].Point(player1); // 15 - 0
+            mySet[2].Point(player1); // 30 - 0
+            mySet[2].Point(player2); // 30 - 15
+            mySet[2].Point(player1); // 40 - 15
+            mySet[2].Point(player1); // Player 1 vinner Gamet
+
+            Assert.Equal(3, mySet.Points(player1));
+            Assert.Equal(0, mySet.Points(player2));
+        }
+
+
         [Fact]
         public void Point_In_Game()
         {
@@ -17,5 +62,7 @@ namespace PadelTest
           //  _games.Point(player1);
 
         }
+
+
     }
 }
